4663be0bd0734868c1e388aa9678e828047b02ce
sneiderg18 <luissneiderg854@gmail.com>
2026-03-18 22:00:41 -0500

    inicio del servidor para trabajar flutter

--- frontend/la_trainer_app/lib/config/api_config.dart
+++ frontend/la_trainer_app/lib/config/api_config.dart
@@ -1,3 +1,3 @@
 class ApiConfig {
-  static const String baseUrl = 'http://192.168.1.14:8000/api';
+  static const String baseUrl = 'https://josue-noncoincident-interferometrically.ngrok-free.dev';
 }
