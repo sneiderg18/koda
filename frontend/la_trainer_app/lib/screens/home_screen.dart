b3c7ffac4b9e8e3c47e14581460fe11773b21f3a
Nick <nickduran01800@gmail.com>
2026-03-21 13:51:02 -0500

    modificacion parcial 2 de login

--- frontend/la_trainer_app/lib/screens/home_screen.dart
+++ frontend/la_trainer_app/lib/screens/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFFF5F7FA),
       appBar: AppBar(
+        
         backgroundColor: Colors.white,
         elevation: 0,
         title: const Text(
